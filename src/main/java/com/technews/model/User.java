88d3acc5840c34c95f5f5234cbc23c003b1061b2
Alex Geis <siegxela@gmail.com>
2023-03-06 11:25:19 -0700

    add Instance Variable-Level Annotations for the User Class

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,18 +1,21 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 // jakarta = javax  in demo - may need to update later
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String username;
+    @Column(unique = true)
     private String email;
     private String password;
+    @Transient
     boolean loggedIn;
 
     private List<Post> posts;
