590351ea9bdc0df0209ca062bf86e615a959649e
Alex Geis <siegxela@gmail.com>
2023-03-09 11:21:49 -0700

    add HomePageController to control user flow of application - add /login and /users/logout mappings to check the user session and redirect accordingly based on loggedIn status

---
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -0,0 +1,46 @@
+package com.technews.controller;
+
+import com.technews.model.User;
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @GetMapping("/login")
+    public String login(Model model, HttpServletRequest request) {
+
+        if (request.getSession(false) != null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @GetMapping("/users/logout")
+    public String logout(HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            request.getSession().invalidate();
+        }
+        return "redirect:/login";
+    }
+}
